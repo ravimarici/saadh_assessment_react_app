ad6c904e98efa35643808593aaa8c772bb22f1c1
saadh <saadh.anwar@maricisolutions.com>
2022-09-15 11:20:24 +0530

    Deleted duplicate header

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Components/Home";
 import SignIn from "./Components/SignIn";
 
 function App() {
- 
   return (
     <div>
-    <Router>
-        <Routes> 
-          <Route path="/home" element={<Home/>} />
-          <Route path="/" element={<SignIn/>} />          
+      <Router>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<SignIn />} />
         </Routes>
-    </Router>
+      </Router>
     </div>
   );
 }
